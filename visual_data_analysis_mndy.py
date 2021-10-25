7b3faff06520f362f342a8bcd70f1a1d9d6fb170
dcndn4 <stokes@iphouse.com>
2021-10-25 17:47:30 -0500

    First run through with initial pieces in place_just starting

---
+++ visual_data_analysis_mndy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 17:32:35 2021
+
+@author: CS_Knit_tinK_SC
+"""
+
+# Visual Data Analysis of Fraudulent Transactions¶
+
+# Your CFO has also requested detailed trends data on specific card holders. 
+# Use the starter notebook to query your database and generate visualizations 
+# that supply the requested information as follows, then add your 
+# visualizations and observations to your markdown report.
+
+#%%
